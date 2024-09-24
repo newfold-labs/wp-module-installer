b1f2d648a512cf5632c189709b601be5ce2d6e40
Allen Benny <allen.officially@gmail.com>
2024-09-24 17:53:37 +0530

    Update PluginInstallTask.php

--- includes/Tasks/PluginInstallTask.php
+++ includes/Tasks/PluginInstallTask.php
@@ -37,18 +37,36 @@ class PluginInstallTask extends Task {
 	private $retries;
 
 	/**
+	 * Boolean if the given plugin is a entitlement.
+	 *
+	 * @var int
+	 */
+	private $is_entitlement;
+
+	/**
+	 * Meta information about a plugin, useful for entitlements.
+	 *
+	 * @var int
+	 */
+	private $meta;
+
+	/**
 	 * PluginInstallTask constructor
 	 *
 	 * @param string  $slug The slug for the Plugin. Ref: includes/Data/Plugins.php for the slugs.
 	 * @param boolean $activate A value of true activates the plugin.
 	 * @param int     $priority Priority of the task, higher the number higher the priority.
 	 * @param int     $retries The number of times the Task has been retried
+	 * @param boolean $is_entitlement If the given plugin is a entitlement
+	 * @param object  $meta Meta information about a plugin
 	 */
-	public function __construct( $slug, $activate, $priority = 0, $retries = 0 ) {
-		$this->slug     = $slug;
-		$this->activate = $activate;
-		$this->priority = $priority;
-		$this->retries  = $retries;
+	public function __construct( $slug, $activate, $priority = 0, $retries = 0, $is_entitlement = false, $meta = array() ) {
+		$this->slug           = $slug;
+		$this->activate       = $activate;
+		$this->priority       = $priority;
+		$this->retries        = $retries;
+		$this->is_entitlement = $is_entitlement;
+		$this->meta           = $meta;
 	}
 
 	/**
