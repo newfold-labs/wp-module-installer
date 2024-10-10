2f855de4f512c6bf553e3652845cf9faa74d63c7
Evan Mullins <evanrm@gmail.com>
2024-10-10 13:11:29 -0400

    version bump to beta.2 and update build files

---
+++ build/1.2.0-beta.2/dataAttrListener.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '45bba5e1c28cf5e9680b');
---
+++ build/1.2.0-beta.2/installer.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'bf1c59a0112d9f2261dd');
