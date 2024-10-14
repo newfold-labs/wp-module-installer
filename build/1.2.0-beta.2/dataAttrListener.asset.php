9de51c8e506acdb37acb61c25b75c84436f0a935
Allen Benny <allen.officially@gmail.com>
2024-10-14 17:35:17 +0530

    Build

--- build/1.2.0-beta.2/dataAttrListener.asset.php
+++ build/1.2.0-beta.2/dataAttrListener.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '45bba5e1c28cf5e9680b');
+<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '6ea17fa75c032203c28b');
--- build/1.2.0-beta.2/installer.asset.php
+++ build/1.2.0-beta.2/installer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'bf1c59a0112d9f2261dd');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c25f129ed18c581691c0');
