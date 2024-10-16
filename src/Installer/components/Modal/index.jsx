235d62f003e1738729d4bb0a806b3e1a57a11bd5
Micah Wood <micah@wpscholar.com>
2024-10-15 20:01:59 -0400

    Update code formatting

--- src/Installer/components/Modal/index.jsx
+++ src/Installer/components/Modal/index.jsx
@@ -1,24 +1,29 @@
 // External Imports
 import apiFetch from '@wordpress/api-fetch';
-import {__, sprintf} from '@wordpress/i18n';
-import {Icon, info} from '@wordpress/icons';
-import {createInterpolateElement, useRef, useState, useEffect} from '@wordpress/element';
+import { __, sprintf } from '@wordpress/i18n';
+import { Icon, info } from '@wordpress/icons';
+import {
+	createInterpolateElement,
+	useRef,
+	useState,
+	useEffect,
+} from '@wordpress/element';
 
 // Internal Imports
-import {loadingInstaller} from '../../static/icons/index';
+import { loadingInstaller } from '../../static/icons/index';
 import {
 	INSTALLER_DIV,
 	installerAPI,
 	pluginInstallHash,
 } from '../../constants';
 
-const Modal = ({
-	               pluginName,
-	               pluginSlug,
-	               pluginURL,
-	               pluginActivate,
-	               pluginProvider,
-               }) => {
+const Modal = ( {
+	pluginName,
+	pluginSlug,
+	pluginURL,
+	pluginActivate,
+	pluginProvider,
+} ) => {
 	/**
 	 * Represents the status of the plugin installation process.
 	 *
@@ -29,46 +34,46 @@ const Modal = ({
 	 * @property {'failed'}     failed     - The plugin installation process failed.
 	 * @property {'completed'}  completed  - The plugin installation process is complete.
 	 */
-	const [pluginStatus, setPluginStatus] = useState('unknown');
+	const [ pluginStatus, setPluginStatus ] = useState( 'unknown' );
 	// const [ isRequestCompleted, setIsRequestCompleted ] = useState( false );
-	const modalRef = useRef(null);
+	const modalRef = useRef( null );
 
-	useEffect(() => {
+	useEffect( () => {
 		installPremiumPlugin();
-	}, [pluginSlug]);
+	}, [ pluginSlug ] );
 
-	const handleKeyDown = (event) => {
-		if (event.key === 'Escape') {
+	const handleKeyDown = ( event ) => {
+		if ( event.key === 'Escape' ) {
 			closeModal();
 		}
 	};
 
-	const handleClickOutside = (event) => {
-		if (modalRef.current && !modalRef.current.contains(event.target)) {
+	const handleClickOutside = ( event ) => {
+		if ( modalRef.current && ! modalRef.current.contains( event.target ) ) {
 			closeModal();
 		}
 	};
 
-	useEffect(() => {
-		document.addEventListener('keydown', handleKeyDown);
-		document.addEventListener('mousedown', handleClickOutside);
+	useEffect( () => {
+		document.addEventListener( 'keydown', handleKeyDown );
+		document.addEventListener( 'mousedown', handleClickOutside );
 
 		return () => {
-			document.removeEventListener('keydown', handleKeyDown);
-			document.removeEventListener('mousedown', handleClickOutside);
+			document.removeEventListener( 'keydown', handleKeyDown );
+			document.removeEventListener( 'mousedown', handleClickOutside );
 		};
-	}, [pluginStatus]);
+	}, [ pluginStatus ] );
 
 	const closeModal = () => {
-		if ('failed' === pluginStatus || 'completed' === pluginStatus) {
-			document.getElementById(INSTALLER_DIV).style.display = 'none';
+		if ( 'failed' === pluginStatus || 'completed' === pluginStatus ) {
+			document.getElementById( INSTALLER_DIV ).style.display = 'none';
 		}
 	};
 
 	const installPremiumPlugin = async () => {
 		try {
-			setPluginStatus('installing');
-			await apiFetch({
+			setPluginStatus( 'installing' );
+			await apiFetch( {
 				url: installerAPI,
 				method: 'POST',
 				headers: {
@@ -82,15 +87,15 @@ const Modal = ({
 					plugin: pluginSlug,
 					provider: pluginProvider,
 				},
-			});
-			setPluginStatus('completed');
-			window.open(pluginURL, '_self');
-		} catch (e) {
-			setPluginStatus('failed');
+			} );
+			setPluginStatus( 'completed' );
+			window.open( pluginURL, '_self' );
+		} catch ( e ) {
+			setPluginStatus( 'failed' );
 		}
 	};
 
-	const helpLink = `${window.NewfoldRuntime.adminUrl}admin.php?page=${window.NewfoldRuntime.plugin.brand}#/help`;
+	const helpLink = `${ window.NewfoldRuntime.adminUrl }admin.php?page=${ window.NewfoldRuntime.plugin.brand }#/help`;
 
 	const errorMessage = createInterpolateElement(
 		__(
@@ -98,51 +103,49 @@ const Modal = ({
 			'wp-module-onboarding'
 		),
 		{
-			a: <a href={helpLink}/>,
+			a: <a href={ helpLink } />,
 		}
 	);
 
 	return (
 		<div className="nfd-installer-modal">
-			<div ref={modalRef} className="nfd-installer-modal__content">
+			<div ref={ modalRef } className="nfd-installer-modal__content">
 				<div className="nfd-installer-modal__content-heading">
-					{__(
+					{ __(
 						'Hold on while we get things setup for you!',
 						'wp-module-installer'
-					)}
+					) }
 				</div>
 				<div className="nfd-installer-modal__content-section">
 					<img
-						src={loadingInstaller}
-						alt={__('Loading Vector.', 'wp-module-onboarding')}
+						src={ loadingInstaller }
+						alt={ __( 'Loading Vector.', 'wp-module-onboarding' ) }
 						className="nfd-installer-modal__content-image"
 					/>
-					{pluginStatus === 'installing' && (
+					{ pluginStatus === 'installing' && (
 						<>
 							<div className="nfd-installer-modal__content-subheading">
-								{sprintf(
+								{ sprintf(
 									/* translators: %s: Plugin Name */
 									__(
 										'Activating… %s',
 										'wp-module-onboarding'
 									),
 									pluginName
-								)}
+								) }
 							</div>
 							<div className="nfd-installer-modal__loader"></div>
 						</>
-					)}
-					{pluginStatus === 'failed' && (
+					) }
+					{ pluginStatus === 'failed' && (
 						<div className="nfd-installer-modal__content-error">
 							<Icon
 								className="nfd-installer-modal__content-error--icon"
-								icon={info}
+								icon={ info }
 							/>
-							<div>
-								{errorMessage}
-							</div>
+							<div>{ errorMessage }</div>
 						</div>
-					)}
+					) }
 				</div>
 			</div>
 		</div>
