8951a26fa00c4e10de24118dce6e9d8a6753521e
Micah Wood <micah@wpscholar.com>
2024-10-16 15:23:04 -0400

    Production build

--- build/1.2.0-beta.3/dataAttrListener.asset.php
+++ build/1.2.0-beta.3/dataAttrListener.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready'), 'version' => 'c4a36b8b9fd6199ae2c6');
+<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '2c7bc1e40668bc567a78');
--- build/1.2.0-beta.3/installer.asset.php
+++ build/1.2.0-beta.3/installer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3e1e0f45b3d4b4235f50');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '63d3d7ef908c925236c1');
