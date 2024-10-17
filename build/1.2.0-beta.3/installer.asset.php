889a796f3ec1f3164120dcda5c1a7d16f4a73e08
arunshenoy99 <devarunshenoy99@gmail.com>
2024-10-17 17:49:14 +0530

    Update build

--- build/1.2.0-beta.3/installer.asset.php
+++ build/1.2.0-beta.3/installer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '63d3d7ef908c925236c1');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ccd0b1b875c7deba74d5');
