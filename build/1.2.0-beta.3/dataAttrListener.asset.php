8d513d668cccda7fb8c539e3bf0be1b09bdb9633
arunshenoy99 <devarunshenoy99@gmail.com>
2024-10-17 18:58:26 +0530

    Update build

--- build/1.2.0-beta.3/dataAttrListener.asset.php
+++ build/1.2.0-beta.3/dataAttrListener.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready'), 'version' => '2c7bc1e40668bc567a78');
+<?php return array('dependencies' => array('wp-dom-ready'), 'version' => 'd688a7c45dc137ed4ceb');
--- build/1.2.0-beta.3/installer.asset.php
+++ build/1.2.0-beta.3/installer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ccd0b1b875c7deba74d5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '20589e8ef523f5b2ed7b');
