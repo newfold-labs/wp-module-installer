a820ab84b4debf805b2ef171de4bb11c95aa35ca
Evan Mullins <evanrm@gmail.com>
2024-10-21 17:46:34 -0400

    add a helper method for rendering installer button to runtime

--- src/Installer/installer.js
+++ src/Installer/installer.js
@@ -40,3 +40,90 @@ const renderModal = ( elementId ) => {
 		}
 	}
 };
+
+// Installer API/helpers
+{
+	const attachToRuntime = () => {
+		// window.NewfoldRuntime.comingSoon = buildObject();
+		window.NewfoldRuntime.installer = buildObject();
+	};
+
+	// attach helper method to runtime
+	const buildObject = () => {
+		return {
+			renderInstallerButton,
+		};
+	};
+
+	/**
+	 * Helper JS method to be used when instantiating buttons to integrate with the installer.
+	 *
+	 * @param {Object} entitlement the entitlement or product
+	 * @param {string} classes     to be added to the button
+	 */
+	const renderInstallerButton = ( entitlement, classes = '' ) => {
+		/*
+		entitlementShape = {
+			"name": "Advanced Reviews",
+			"url": "https://example.com", // redirect on completion
+			"cta": {
+			"text": "Manage",
+			"url": "{siteUrl}/wp-admin/admin.php?page=bluehost#/example"
+			},
+			"plsProviderName": "yith", // premium provider
+			"plsSlug": "yith-woocommerce-advanced-reviews", // premium id
+			"download": null, // free download url
+			"slug": "example",
+			"basename": "example.com/example.php",
+		};
+		*/
+		if (
+			! entitlement ||
+			! entitlement.cta.url ||
+			! entitlement.cta.text ||
+			! entitlement.basename ||
+			! entitlement.name
+		) {
+			return;
+		}
+
+		let buttonHTML = `<button
+			class="${ classes }"
+			href="${ renderCTAUrl( entitlement.cta.url ) }"
+			data-nfd-installer-plugin-basename="${ entitlement.basename }"
+			data-nfd-installer-plugin-name="${ entitlement.name }"
+		`;
+		// premium attributes
+		if ( entitlement.plsProviderName && entitlement.plsSlug ) {
+			buttonHTML += `
+			data-nfd-installer-pls-slug="${ entitlement.plsSlug }"
+			data-nfd-installer-pls-provider="${ entitlement.plsProviderName }"
+			`;
+		}
+		// free attributes
+		if ( entitlement.download ) {
+			buttonHTML += `
+			data-nfd-installer-download-url="${ entitlement.download }"
+			`;
+		}
+		buttonHTML += `>${ entitlement.cta.text }</button>`; // close button element
+		return buttonHTML;
+	};
+
+	/**
+	 * Helper method to clean cta urls for use - replacing {siteUrl} with actual
+	 * @param {string} url the url to transform
+	 */
+	const renderCTAUrl = ( url ) => {
+		if ( ! window.NewfoldRuntime || ! window.NewfoldRuntime.siteUrl ) {
+			// fallback to site relative url if no base_url is found
+			return url.replace( '{siteUrl}', '' );
+		}
+		return url.replace( '{siteUrl}', window.NewfoldRuntime.siteUrl );
+	};
+
+	window.addEventListener( 'DOMContentLoaded', () => {
+		attachToRuntime();
+	} );
+
+}
