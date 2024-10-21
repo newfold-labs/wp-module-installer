ac5c53b82bf553108e70a215d75bde7204082cda
Allen Benny <allen.officially@gmail.com>
2024-10-21 17:34:25 +0530

    Efficient

--- includes/WPAdmin/Listeners/InstallerListener.php
+++ includes/WPAdmin/Listeners/InstallerListener.php
@@ -15,10 +15,45 @@ class InstallerListener {
 	 */
 	public function __construct() {
 		// Hook to enqueue installer scripts
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_installer_script' ) );
+		add_action( 'newfold/installer/enqueue_scripts', array( $this, 'enqueue_installer_scripts' ) );
 
 		// Hook to listen to premium plugin activation
 		$this->listen_for_premium_plugin_activation();
+	}
+
+	/**
+	 * Enqueues all the installer scripts that are required.
+	 * The Data Attribute Listener Script
+	 * The Modal UI with React that installs the plugin
+	 *
+	 * @return void
+	 */
+	public function enqueue_installer_scripts() {
+		$this->enqueue_data_attr_listener();
+		$this->enqueue_installer_react_script();
+	}
+
+	/**
+	 * Enqueues the data-* attribute listener script.
+	 *
+	 * @return void
+	 */
+	public function enqueue_data_attr_listener() {
+		$asset_file = NFD_INSTALLER_BUILD_DIR . '/dataAttrListener.asset.php';
+
+		if ( is_readable( $asset_file ) ) {
+			$asset = include $asset_file;
+
+			wp_register_script(
+				'nfd-installer-data-attr-listener',
+				NFD_INSTALLER_BUILD_URL . '/dataAttrListener.js',
+				array_merge( $asset['dependencies'], array() ),
+				$asset['version'],
+				true
+			);
+
+			wp_enqueue_script( 'nfd-installer-data-attr-listener' );
+		}
 	}
 
 	/**
@@ -26,7 +61,7 @@ class InstallerListener {
 	 *
 	 * @return void
 	 */
-	public function enqueue_installer_script() {
+	public function enqueue_installer_react_script() {
 		$asset_file = NFD_INSTALLER_BUILD_DIR . '/installer.asset.php';
 
 		if ( is_readable( $asset_file ) ) {
