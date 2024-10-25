427c7fd777cea335a5169eae5506938625b97b4c
Micah Wood <micah@wpscholar.com>
2024-10-25 10:26:21 -0400

    Production build

--- build/1.2.2/installer.asset.php
+++ build/1.2.2/installer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '806142ff4b027fb6e92b');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '09a165a23da81a348e0d');
