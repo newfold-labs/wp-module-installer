d143469fcf7e871c1ebdcf7b9dfbf9adceb9e4f0
Vara <varaprasadmaruboina@gmail.com>
2025-02-24 19:42:11 +0530

    Update Translations

--- build/1.2.2/installer.asset.php
+++ build/1.2.2/installer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '09a165a23da81a348e0d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '551cba0089e015aea478');
