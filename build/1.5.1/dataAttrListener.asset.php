8f535ce48f908a88dc2e925384c7b14a2f62adb2
Evan Mullins <evanrm@gmail.com>
2025-04-23 15:46:11 -0400

    Version updated from 1.5.1 to 1.6.0

--- build/1.5.1/dataAttrListener.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-dom-ready'), 'version' => 'b208b9a3938b304abbde');
--- build/1.5.1/installer.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'b7a0c70a39e119854eda');
