1476d65e77570c14112fc0f8d06c03cf2c3e1008
Evan Mullins <evanrm@gmail.com>
2025-07-30 08:35:30 -0400

    fix assets path - remove double `/`

--- includes/Data/Constants.php
+++ includes/Data/Constants.php
@@ -21,7 +21,7 @@ class Constants {
 			define( 'NFD_INSTALLER_BUILD_DIR', dirname( __DIR__, 2 ) . '/build/' . NFD_INSTALLER_VERSION );
 		}
 		if ( ! defined( 'NFD_INSTALLER_BUILD_URL' && defined( 'NFD_INSTALLER_VERSION' ) ) ) {
-			define( 'NFD_INSTALLER_BUILD_URL', $container->plugin()->url . '/vendor/newfold-labs/wp-module-installer/build/' . NFD_INSTALLER_VERSION );
+			define( 'NFD_INSTALLER_BUILD_URL', $container->plugin()->url . 'vendor/newfold-labs/wp-module-installer/build/' . NFD_INSTALLER_VERSION );
 		}
 	}
 }
