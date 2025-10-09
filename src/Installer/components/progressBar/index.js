0c3a2be339a3ce0979397205ba69fbe5eef75eb2
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-10-09 19:25:25 +0100

    lint js

--- src/Installer/components/progressBar/index.js
+++ src/Installer/components/progressBar/index.js
@@ -3,7 +3,7 @@ import { memo } from '@wordpress/element';
 export const ProgressBar = memo( ( { completed, total } ) => {
 	const percent = total ? Math.round( ( completed / total ) * 100 ) : 0;
 	return (
-		<div className={`nfd-progress-bar nfd-progress-bar-${ percent }`}>
+		<div className={ `nfd-progress-bar nfd-progress-bar-${ percent }` }>
 			<div
 				className="nfd-progress-bar-inner"
 				data-percent={ percent }
