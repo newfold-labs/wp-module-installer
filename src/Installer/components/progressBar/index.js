d349e723725c12f1bd3981e7872efd06764bdf0e
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-10-09 16:11:20 +0100

    New: Progressbar component

---
+++ src/Installer/components/progressBar/index.js
@@ -0,0 +1,14 @@
+import { memo } from '@wordpress/element';
+
+export const ProgressBar = memo( ( { completed, total } ) => {
+	const percent = total ? Math.round( ( completed / total ) * 100 ) : 0;
+	return (
+		<div className={`nfd-progress-bar nfd-progress-bar-${ percent }`}>
+			<div
+				className="nfd-progress-bar-inner"
+				data-percent={ percent }
+				style={ { width: `${ percent }%` } }
+			/>
+		</div>
+	);
+} );
